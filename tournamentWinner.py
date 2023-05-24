e7f0a52c4a2cc31f15c28f552bd931c4fe6f2ab8
bmmukhridin <bmmukhridin@icloud.com>
2023-05-24 14:01:11 -0700

    done

--- tournamentWinner.py
+++ tournamentWinner.py
@@ -1,11 +1,27 @@
 competitions1 = [["HTML", "C#"],["C#", "Python"],["Python", "HTML"]]
-
 results  = [0,0,1]
+
+scores = {}
+
+def updateScores(team, points, scores):
+    if team not in scores:
+        scores[team] = 0
+
+    scores[team] +=points
 
 for idx, competitions in enumerate(competitions1):
     result = results[idx]
     homeTeam, awayTeam = competitions
-    round1 = [homeTeam, awayTeam]
+    winnigTeam = [homeTeam, awayTeam]
+    winnigTeam.pop(result)
+
+    for i in winnigTeam:
+        updateScores(i, 3, scores)
+
+
+resulys_new = max(scores, key=scores.get)
+
+resulys_new
 
 
 
