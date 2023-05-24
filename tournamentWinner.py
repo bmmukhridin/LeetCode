4bc550a6742be70a4e89ba0cc1a479d8be04fa98
bmmukhridin <bmmukhridin@icloud.com>
2023-05-24 00:13:41 -0700

    started

---
+++ tournamentWinner.py
@@ -0,0 +1,11 @@
+competitions1 = [["HTML", "C#"],["C#", "Python"],["Python", "HTML"]]
+
+results  = [0,0,1]
+
+for idx, competitions in enumerate(competitions1):
+    result = results[idx]
+    homeTeam, awayTeam = competitions
+    round1 = [homeTeam, awayTeam]
+
+
+
